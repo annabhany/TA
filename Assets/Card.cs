5ae41eda19c5ec989e8f534c715875ece321abb1
Reynold Erwandi <reynolderwandi@gmail.com>
2016-12-12 14:17:21 +0700

    Cocok Kartu Gameplay

---
+++ Assets/Card.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour {
+
+	public Sprite front;
+	public Sprite back;
+	Image image;
+	Button button;
+
+	bool isOpen = false;
+
+	void Awake () {
+		image = GetComponent<Image> ();
+		button = GetComponent<Button> ();
+	}
+
+	void Update () {
+		
+	}
+
+	public void Open() {
+		image.sprite = front;
+		isOpen = true;
+		button.interactable = false;
+	}
+
+	public void Close() {
+		image.sprite = back;
+		isOpen = false;
+		button.interactable = true;
+	}
+
+	public void Toggle() {
+		if (isOpen) {
+			Close ();
+		} else {
+			CocokKartuManager.Instance.CardChoose (this);
+			Open ();
+		}
+	}
+}
